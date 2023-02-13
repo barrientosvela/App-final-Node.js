709b9add2e4ca969b069ed971976bf7bf90950f8
barrientosvela <jbarrientosvela@gmail.com>
2023-02-13 20:56:26 +0100

    add changes

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const path = require('path');
+require('dotenv').config()
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+const port = process.env.PORT
+
+//Conexión a base de datos
+const mongoose = require('mongoose');
+//Variables que tendremos siempre:
+//Lo correcto será declararlas EN VARIABLES DE ENTORNO
+//para que nadie vea directamente nuestras credenciales
+const uri = `mongodb+srv://${process.env.BD_USER}:${process.env.BD_PASSWORD}@cluster0.ccao8rx.mongodb.net/${process.env.BD_NAME}?retryWrites=true&w=majority`; //URL de conexión, que completaremos luego
+
+mongoose.connect(uri,
+  { useNewUrlParser: true, useUnifiedTopology: true }
+)
+  .then(() => console.log('Base de datos conectada'))
+  .catch(e => console.log(e))
+//Middleware
+app.use(express.static(__dirname + '/public'));
+
+//Motor de plantillas
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+
+//Peticiones básicas HTTP
+app.use('/', require('./router/rutas'))
+app.use('/pokemon', require('./router/pokemon'))
+app.use('/entrenador', require('./router/entrenador'))
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+app.use((req, res) => {
+  res.status(404).render("404", {
+    titulo: "Página no encontrada"
+  })
+})
---
+++ router/rutas.js
@@ -0,0 +1,41 @@
+const express = require('express'); //Requerimos Express
+const Pokemon = require('../models/pokemon');
+const Entrenador = require('../models/entrenador')
+const router = express.Router();
+
+
+// Ahora, CORTAMOS del fichero principal 01-express.js
+// las dos rutas que tenemos: la principal ( / ) y la 
+// de contactos ( /contaco )
+// Importante que ya no usaremos el app.get(...), ahora
+//vamos a utilizar las rutas, por lo que deberemos poner:
+router.get('/', (req, res) => {
+    res.render("index")
+})
+router.get('/contacto', (req, res) => {
+    res.render("contacto", { tituloContacto: "Estamos en contacto de manera dinámica!!" })
+})
+
+router.post('/', async (req, res) => {
+    const body = req.body
+    try {
+        const pokemonDB = new Pokemon(body)
+        await pokemonDB.save()
+        res.redirect('/pokemon')
+    } catch (error) {
+        console.log('Error: ', error)
+    }
+})
+router.post('/', async (req, res) => {
+    const body = req.body
+    try {
+        const entrenadorDB = new Entrenador(body)
+        await entrenadorDB.save()
+        res.redirect('/entrenador')
+    } catch (error) {
+        console.log('Error: ', error)
+    }
+})
+
+// Por último, vamos a exportarlo:
+module.exports = router;
