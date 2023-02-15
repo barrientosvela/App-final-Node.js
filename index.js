9855a0f7237650aaab996b8dac1c183dd8c38451
barrientosvela <jbarrientosvela@gmail.com>
2023-02-15 21:01:19 +0100

    add changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const path = require('path');
 require('dotenv').config()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
